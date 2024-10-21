e534d4e265865d83e9b5681ce3a1fb1062206ccc
shxl2021 <1693847306@qq.com>
2024-10-22 00:16:46 +0800

    jetcache集成spring redisson依赖改为独立yml格式

--- spring-boot-parent/spring-boot-jetcache-mvc/src/main/java/top/boking/springbootjetcachemvc/config/CacheConfiguration.java
+++ spring-boot-parent/spring-boot-jetcache-mvc/src/main/java/top/boking/springbootjetcachemvc/config/CacheConfiguration.java
@@ -1,6 +1,11 @@
 package top.boking.springbootjetcachemvc.config;
 
 import com.alicp.jetcache.anno.config.EnableMethodCache;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -11,4 +16,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableMethodCache(basePackages = "top.boking.springbootjetcachemvc")
 public class CacheConfiguration {
+    /*
+    @Bean(destroyMethod = "shutdown")
+    @ConditionalOnMissingBean
+    public RedissonClient redisson() {
+        Config config = new Config();
+        *//*
+        SingleServerConfig singleServerConfig = config.useSingleServer();
+        singleServerConfig.setAddress("redis://127.0.0.1:7000");
+        *//*
+        config.useClusterServers()
+                // 添加多个集群节点地址，至少要添加一个主节点地址
+                .addNodeAddress("redis://127.0.0.1:7000",
+                        "redis://127.0.0.1:7001",
+                        "redis://127.0.0.1:7002");
+        return Redisson.create(config);
+    }
+    */
 }
