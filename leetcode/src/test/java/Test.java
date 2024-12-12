13677afbfd51bdd9e6d1dff3c40effdc92724d0f
sxl <1693847306@qq.com>
2024-12-12 20:17:05 +0800

    ..

--- leetcode/src/test/java/Test.java
+++ leetcode/src/test/java/Test.java
@@ -1,8 +1,4 @@
 public class Test {
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        ListNode listNode = ListNode.of(new int[]{1, 2});
-        ListNode listNode1 = solution.removeNthFromEnd(listNode, 2);
-        ListNode.print(listNode1);
     }
 }
