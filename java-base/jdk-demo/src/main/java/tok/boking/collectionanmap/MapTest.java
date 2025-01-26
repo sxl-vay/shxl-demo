0cefe81932ce78267662af5716f73ca7efa1ddc6
史祥连 <1693847306@qq.com>
2025-01-26 16:18:42 +0800

    Test

--- java-base/jdk-demo/src/main/java/tok/boking/collectionanmap/MapTest.java
+++ java-base/jdk-demo/src/main/java/tok/boking/collectionanmap/MapTest.java
@@ -1,8 +1,32 @@
 package tok.boking.collectionanmap;
+
+import tok.boking.enumdemo.TestEnum;
+
 @SuppressWarnings("warning")
 public class MapTest {
+    /**
+     * empty
+     */
+    public void get() {
+
+    }
+
+    public void get(TestEnum testEnum) {
+
+    }
+
+    public String get(String s) {
+        return s;
+    }
+
+    public boolean get(Boolean b, String s) {
+        return b;
+    }
+
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++)                                                                                      ;
-        System.out.println(111);
+        MapTest mapTest = new MapTest();
+        mapTest.get(TestEnum.A);
+        mapTest.get(TestEnum.B);
+        mapTest.get(TestEnum.C);
     }
 }
